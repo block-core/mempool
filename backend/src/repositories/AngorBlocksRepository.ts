210a7230df9adfd434070bc765f06357a2363bba
NostrDev <support@nostrdev.com>
2025-01-02 15:37:34 +0200

    chore: removes redundant code

--- backend/src/repositories/AngorBlocksRepository.ts
+++ backend/src/repositories/AngorBlocksRepository.ts
@@ -37,35 +37,6 @@ class AngorBlocksRepository {
       throw error;
     }
   }
-
-
-  public async $markBlockAsIndexed(blockHeight: number, blockHash: string): Promise<void> {
-    try {
-      await DB.query(
-        `INSERT INTO angor_blocks (block_height, block_hash) VALUES (?, ?)`, [blockHeight, blockHash])
-    } catch (error) {
-      logger.err(
-        'Error marking block as indexed. Reason: ' + (error instanceof Error ? error.message : error)
-      );
-    }
-  }
-
-  public async isBlockIndexed(blockHeight: number, blockHash: string): Promise<boolean> {
-    try {
-      const result = await DB.query(
-        `SELECT * FROM angor_blocks WHERE block_height = ? AND block_hash = ? LIMIT 1`, [blockHeight, blockHash]
-      );
-
-      const rows = result[0] as RowDataPacket;
-
-      return rows.length > 0;
-    } catch (error) {
-      logger.err(
-        'Error checking if block is indexed. Reason: ' + (error instanceof Error ? error.message : error)
-      );
-      return false;
-    }
-  }
 }
 
 export default new AngorBlocksRepository();
