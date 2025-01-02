7bd04214f1946852abfdeef620cd1bc9660b4081
NostrDev <support@nostrdev.com>
2025-01-02 15:09:10 +0200

    (feat: angor-optimise): refactors angor indexing

--- backend/src/repositories/AngorBlocksRepository.ts
+++ backend/src/repositories/AngorBlocksRepository.ts
@@ -3,6 +3,42 @@ import logger from "../logger";
 import { RowDataPacket } from "mysql2/typings/mysql";
 
 class AngorBlocksRepository {
+  public async $updateLatestIndexedBlock(blockHeight: number, blockHash: string): Promise<void> {
+    try {
+      await DB.query(
+        `INSERT INTO angor_blocks (id, block_height, block_hash)
+        VALUES (1, ?, ?)
+        ON DUPLICATE KEY UPDATE
+            block_height = ?,
+            block_hash = ?`, [blockHeight, blockHash, blockHeight, blockHash]
+      );
+    } catch (error) {
+      logger.err(
+        'Error updating latest indexed block. Reason: ' + (error instanceof Error ? error.message : error)
+      );
+    }
+  }
+
+  public async $getLatestIndexedBlock(): Promise<number | null> {
+    try {
+      const [rows]= await DB.query(
+        `SELECT block_height FROM angor_blocks WHERE id = 1`
+      ) as RowDataPacket[][];
+
+      if (rows.length === 0) {
+        return null;
+      } else {
+        return rows[0].block_height as number;
+      }
+    } catch (error) {
+      logger.err(
+        'Error getting latest indexed block. Reason: ' + (error instanceof Error ? error.message : error)
+      );
+      throw error;
+    }
+  }
+
+
   public async $markBlockAsIndexed(blockHeight: number, blockHash: string): Promise<void> {
     try {
       await DB.query(
